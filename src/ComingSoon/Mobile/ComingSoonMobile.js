8d84375c3b707287a0c4ce49300ee075e738a5ab
Sean <99774652+Minimage@users.noreply.github.com>
2023-05-08 14:18:32 +0000

    Finished error handling for mobile

--- src/ComingSoon/Mobile/ComingSoonMobile.js
+++ src/ComingSoon/Mobile/ComingSoonMobile.js
@@ -3,8 +3,30 @@ import person from "../../icons/hero-desktop.jpg";
 import background from "../../icons/bg-pattern-desktop.svg";
 import logo from "../../icons/logo.svg";
 import btn_bg from "../../icons/icon-arrow.svg";
+import errorIcon from "../../icons/icon-error.svg";
+import { useState } from "react";
 
 export const ComingSoonMobile = () => {
+  const [email, setEmail] = useState("");
+  const [error_msg, setErrorMsg] = useState("");
+  const [error_icon, setErrorIcon] = useState(false);
+  const contains = ["@", "."];
+
+  const value = contains.every((item) => email.includes(item));
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    if (value === false) {
+      console.log("Field doesnt contain correct values");
+      setErrorMsg("Please enter a valid email");
+      setErrorIcon(true);
+    } else {
+      setErrorMsg("");
+      setErrorIcon(false);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div
@@ -22,34 +44,55 @@ export const ComingSoonMobile = () => {
               <br /> SOON
             </h1>
 
+            <p>
+              {/* {word.includes(contains[0]) && word.includes(contains[1])
+                ? "yes"
+                : "no"} */}
+            </p>
             <p className={styles.text}>
               Hello fellow shoppers! We're currently building our new fashion
               store. Add your email below to stay up-to-date with announcements
               and our launch deals.
             </p>
 
-            {/* <label className="label"> */}
-            <form>
-              <input
-                className={styles.inputField}
-                type="email"
-                placeholder="Email Address"
-              />
+            <div className={styles.bottomWrapper}>
+              <form>
+                <input
+                  className={styles.inputField}
+                  type="email"
+                  placeholder="Email Address"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
 
-              <button
-                style={{
-                  background: `url(${btn_bg})`,
-                  backgroundImage:
-                    "linear-gradient(135deg,hsl(0, 80%, 86%),hsl(0, 74%, 74%))",
-                  backgroundRepeat: "no-repeat",
-                  backgroundPosition: "50%",
-                }}
-                className={styles.submit}
-              >
-                {<img src={btn_bg} />}
-              </button>
-            </form>
-            {/* </label> */}
+                {
+                  <img
+                    className={
+                      error_icon
+                        ? `${styles.show} ${styles.errorBtn}`
+                        : `${styles.hide}`
+                    }
+                    src={errorIcon}
+                  />
+                }
+                <button
+                  onClick={handleClick}
+                  style={{
+                    background: `url(${btn_bg})`,
+                    backgroundImage:
+                      "linear-gradient(135deg,hsl(0, 80%, 86%),hsl(0, 74%, 74%))",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "50%",
+                  }}
+                  className={styles.submit}
+                >
+                  {<img src={btn_bg} />}
+                </button>
+                {/* <div className={styles.break}></div> */}
+              </form>
+              <p className={styles.errorMsg}>{error_msg}</p>
+            </div>
           </div>
         </div>
       </div>
