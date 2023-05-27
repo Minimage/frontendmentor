90ff6112077f340ce4300c393a7f6f4c71889220
Minimage <seancampbell914.com>
2023-05-27 08:20:32 -0400

    pushing to main effects vercel

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   return (
     // Testing to see what happens to vercel if I push to main
+    // editing main does interfer with vercel...
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
